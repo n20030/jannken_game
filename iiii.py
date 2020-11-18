bbe4ae54ac78ecc3c6ac44da63c92e3a953baff9
bob <n20030@std.it-college.ac.jp>
2020-11-18 12:06:41 +0900

    指定した番号以外を選択したときすばやく処理を終えるようにした

--- iiii.py
+++ iiii.py
@@ -13,48 +13,49 @@ print("hello! It's rock-paper-scissors time")
 sl1()
 jan = input("-- Please enter the number --  \n 1. rock \n 2. scissors \n 3. paper  ")
 sl1()
-print('You chose number ' + str(jan) +":")
-
-#Game start call
-sl2()
-print("Rock, ")
-sl1()
-print("Paper,")
-sl1()
-print("Scissors,")
-sl1()
-print("Go!!")
-sl1()
-
-
-def chose_cpu():
-	print("I chose " + random_cpu)
-
-import random
-a = 'rock'
-b = 'scissors'
-c = 'paper'
-cpu = [a , b , c]
-random_cpu = random.choice(cpu)
-bout = str(jan) + random_cpu
-sl2()
-#print(bout)
-
-if bout == '1' + a or bout == '2' + b or bout == '3' + c :
-	chose_cpu()
-	print('draw!!')
-
-elif bout == '1' + b or bout == '2' + c or bout == '3' + a :
-	chose_cpu()
-	print('Your win!!')
-
-elif bout == '1' + c or bout == '2' + a or bout == '3' + b :
-	chose_cpu()
-	print('Your defeat...')
-
-else :
+if jan < 1 or jan > 3 :
 	print("Choose one of 1,2,3 ")
 
+else :
+	
 
-print('test')
-print('test')
+	print('You chose number ' + str(jan) +":")
+
+	#Game start call
+	sl2()
+	print("Rock, ")
+	sl1()
+	print("Paper,")
+	sl1()
+	print("Scissors,")
+	sl1()
+	print("Go!!")
+	sl1()
+
+
+	def chose_cpu():
+		print("I chose " + random_cpu)
+
+	import random
+	a = 'rock'
+	b = 'scissors'
+	c = 'paper'
+	cpu = [a , b , c]
+	random_cpu = random.choice(cpu)
+	bout = str(jan) + random_cpu
+	sl2()
+	#print(bout)
+
+	if bout == '1' + a or bout == '2' + b or bout == '3' + c :
+		chose_cpu()
+		print('draw!!')
+
+	elif bout == '1' + b or bout == '2' + c or bout == '3' + a :
+		chose_cpu()
+		print('Your win!!')
+
+	else :
+		chose_cpu()
+		print('Your defeat...')
+
+
